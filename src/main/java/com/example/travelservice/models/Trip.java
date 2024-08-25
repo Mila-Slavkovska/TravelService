a12537e981f4c67d7ca100fd20f011c5a501c2dd
Mila-Slavkovska <milaslavkovska@gmail.com>
2024-08-25 15:32:50 +0200

    Added Trip CRUD operations

--- src/main/java/com/example/travelservice/models/Trip.java
+++ src/main/java/com/example/travelservice/models/Trip.java
@@ -1,9 +1,9 @@
 package com.example.travelservice.models;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
 import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
@@ -16,7 +16,9 @@ public class Trip {
     Long Id;
 
     String name;
+    @Min(value = 0, message = "Budget cannot be a negative number")
     Double budget;
+    @Min(value = 0, message = "Number of people cannot be a negative number")
     Integer numPeople;
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     LocalDateTime date_from;
@@ -24,19 +26,19 @@ public class Trip {
     LocalDateTime date_to;
     @ManyToMany
     List<Attractions> attractions;
-    @ManyToOne
-    Hotel hotel;
+    @ManyToMany
+    List<Accommodation> accommodations;
 
     public Trip() {
     }
 
-    public Trip(String name, Double budget, Integer numPeople, LocalDateTime date_from, LocalDateTime date_to, List<Attractions> attractions, Hotel hotel) {
+    public Trip(String name, Double budget, Integer numPeople, LocalDateTime date_from, LocalDateTime date_to, List<Attractions> attractions, List<Accommodation> accommodations) {
         this.name = name;
         this.budget = budget;
         this.numPeople = numPeople;
         this.date_from = date_from;
         this.date_to = date_to;
         this.attractions = new ArrayList<>();
-        this.hotel = hotel;
+        this.accommodations = new ArrayList<>();
     }
 }
---
+++ src/main/java/com/example/travelservice/service/Implementations/TripServiceImpl.java
@@ -0,0 +1,69 @@
+package com.example.travelservice.service.Implementations;
+
+import com.example.travelservice.models.Attractions;
+import com.example.travelservice.models.Accommodation;
+import com.example.travelservice.models.Trip;
+import com.example.travelservice.models.dto.TripDto;
+import com.example.travelservice.models.exceptions.TripNotFoundException;
+import com.example.travelservice.repository.TripRepository;
+import com.example.travelservice.service.TripService;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TripServiceImpl implements TripService {
+    private final TripRepository tripRepository;
+
+    public TripServiceImpl(TripRepository tripRepository) {
+        this.tripRepository = tripRepository;
+    }
+
+    @Override
+    public List<Trip> findAll() {
+        return this.tripRepository.findAll();
+    }
+
+    @Override
+    public List<Trip> findByName(String name) {
+        return this.tripRepository.findAllByNameContainingIgnoreCase(name);
+    }
+
+    @Override
+    public Optional<Trip> findById(Long id) {
+        return this.tripRepository.findById(id);
+    }
+
+    @Override
+    public Optional<Trip> save(TripDto tripDto) {
+        //TODO: handle hotels and attractions when CRUD is implemented
+        List<Attractions> attractions = new ArrayList<>();
+        List<Accommodation> accommodations = new ArrayList<>();
+        Trip trip = new Trip(tripDto.name(), tripDto.budget(), tripDto.numPeople(), tripDto.date_from(), tripDto.date_to(), attractions, accommodations);
+        return Optional.of(this.tripRepository.save(trip));
+    }
+
+    @Override
+    public Optional<Trip> edit(Long id, TripDto tripDto) {
+        Trip trip = this.tripRepository.findById(id)
+                .orElseThrow(() -> new TripNotFoundException(id));
+
+        trip.setName(tripDto.name() != null ? tripDto.name() : trip.getName());
+        trip.setBudget(tripDto.budget() != null ? tripDto.budget() : trip.getBudget());
+        trip.setNumPeople(tripDto.numPeople() != null ? tripDto.numPeople() : trip.getNumPeople());
+        trip.setDate_from(tripDto.date_from() != null ? tripDto.date_from() : trip.getDate_from());
+        trip.setDate_to(tripDto.date_to() != null ? tripDto.date_to() : trip.getDate_to());
+        // TODO: use Accommodations and Attractions service to map list of ids to list of entities
+        trip.setAccommodations(new ArrayList<>());
+        trip.setAttractions(new ArrayList<>());
+
+        return Optional.of(this.tripRepository.save(trip));
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        this.tripRepository.deleteById(id);
+    }
+}

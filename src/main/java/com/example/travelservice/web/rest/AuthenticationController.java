813d9f71bcdabe04bf1e39784d199651a38eb318
Mila-Slavkovska <milaslavkovska@gmail.com>
2024-09-03 20:21:41 +0200

    Add user authentication [BE]

---
+++ src/main/java/com/example/travelservice/web/rest/AuthenticationController.java
@@ -0,0 +1,44 @@
+package com.example.travelservice.web.rest;
+
+import com.example.travelservice.models.dto.LoginUserDto;
+import com.example.travelservice.models.dto.RegisterUserDto;
+import com.example.travelservice.models.User;
+import com.example.travelservice.models.responses.LoginResponse;
+import com.example.travelservice.service.auth.AuthenticationService;
+import com.example.travelservice.service.auth.JwtService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping("/api/auth")
+@RestController
+//@CrossOrigin(origins = {"http://localhost:3000", "http://localhost:3001"})
+public class AuthenticationController {
+    private final JwtService jwtService;
+
+    private final AuthenticationService authenticationService;
+
+    public AuthenticationController(JwtService jwtService, AuthenticationService authenticationService) {
+        this.jwtService = jwtService;
+        this.authenticationService = authenticationService;
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<User> register(@RequestBody RegisterUserDto registerUserDto) {
+        User registeredUser = authenticationService.signup(registerUserDto);
+
+        return ResponseEntity.ok(registeredUser);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> authenticate(@RequestBody LoginUserDto loginUserDto) {
+        User authenticatedUser = authenticationService.authenticate(loginUserDto);
+
+        String jwtToken = jwtService.generateToken(authenticatedUser);
+
+        LoginResponse loginResponse = new LoginResponse();
+        loginResponse.setToken(jwtToken);
+        loginResponse.setExpiresIn(jwtService.getExpirationTime());
+
+        return ResponseEntity.ok(loginResponse);
+    }
+}
